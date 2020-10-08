4c1f7ad993167b593812034f18cd9036e3fb0db1
Priyanshu Barnwal <35839635+priyanshu2612@users.noreply.github.com>
2020-10-08 23:14:31 +0530

    Add files via upload

---
+++ loader.css
@@ -0,0 +1,298 @@
+.mediaViewInfo {
+	--web-view-name: loader;
+	--web-view-id: loader;
+	--web-scale-on-resize: true;
+	--web-center-horizontally: true;
+	--web-center-vertically: true;
+	--web-enable-deep-linking: true;
+}
+:root {
+	--web-view-ids: loader;
+}
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	border: none;
+}
+
+#lp{
+
+	position: absolute;
+	transform: translateX(-50%) translateY(-50%);
+	top:50%;
+	left:50%;
+	width: 1260px;
+	height: 616px;
+	background-color: rgba(255,255,255,1);
+	overflow: hidden;
+	--web-view-name: loader;
+	--web-view-id: loader;
+	--web-scale-on-resize: true;
+	--web-center-horizontally: true;
+	--web-center-vertically: true;
+	--web-enable-deep-linking: true;
+	animation-name: examplex;
+    animation-duration: .4s;
+    animation-fill-mode: forwards; animation-delay: 2.7s;
+}
+@keyframes examplex {
+  0%   {opacity: 1;}
+  100% {opacity: 0;}
+}
+#loader {
+	position: absolute;
+	
+	width: 100vw;
+	height: 100vh;
+	background-color: rgba(255,255,255,1);
+	overflow: hidden;
+	--web-view-name: loader;
+	--web-view-id: loader;
+	--web-scale-on-resize: true;
+	--web-center-horizontally: true;
+	--web-center-vertically: true;
+	--web-enable-deep-linking: true;
+}
+
+#Digital_Product_Designer {
+	position: absolute;
+	left: 524px;
+	top: 280px;
+	overflow: visible;
+	width: 332px;
+	white-space: nowrap;
+	text-align: left;
+	font-family: Muli;z-index: -1;
+	font-style: normal;
+	font-weight: lighter;
+	font-size: 30px;
+	color: rgba(35,35,35,1);animation-name: example;
+    animation-duration: .6s;
+    animation-fill-mode: forwards; animation-delay: 2.3s;
+}
+@keyframes example {
+  0%   {left:524px; top:280px;}
+  100% {left:524px; top:314px;}
+}
+.Rectangle_468 {
+	position: absolute;
+	overflow: visible;
+	width: 370px;
+	height: 54px;
+	left: 510px;
+	top: 265px;
+}
+.Rectangle_467 {
+	position: absolute;
+	overflow: visible;
+	width: 64px;
+	height: 64px;
+	left: 100%;
+	top: 100%;
+	animation-name: example1;
+	animation-duration: .2s;
+	animation-fill-mode: forwards;
+	animation-delay: 1.5s;
+}
+@keyframes example1 {
+  0%   {left:433px; top:280px;}
+  100% {left:443px; top:290px;}
+}
+.Rectangle_418 {
+	filter: drop-shadow(0px 3px 6px rgba(0, 0, 0, 0.161));
+	position: absolute;
+	overflow: visible;
+	width: 82px;
+	height: 82px;
+	left: 100%;
+	top: 100%;
+	animation-name: example2;
+    animation-duration: .7s;animation-fill-mode: forwards;animation-delay: 0.8s;
+}
+@keyframes example2 {
+  0%   { outline-color: blue; outline-width: 1px; left:573px; top:280px;}
+  100% { fill: blue; left:433px; top:280px;}
+}
+
+.Rectangle_blue {
+	filter: drop-shadow(0px 3px 6px rgba(0, 0, 0, 0.161));
+	position: absolute;
+	overflow: visible;
+	width: 100%;
+	height: 100%;
+	left: 100%;
+	top: 100%;
+	animation-name: exampleblue;
+	animation-duration: 5s;
+	animation-fill-mode: forwards;
+	animation-delay: 2.9s;
+}
+@keyframes exampleblue {
+  0%   { left:0px; top:100%;}
+  100% { left:0px; top:-1500%;}
+}
+
+
+.Rectangle_yellow {
+	filter: drop-shadow(0px 3px 6px rgba(0, 0, 0, 0.161));
+	position: absolute;
+	overflow: visible;
+	width: 100%;
+	height: 100%;
+	left: 100%;
+	top: 100%;
+	animation-name: exampleblue;
+	animation-duration: 5s;
+	animation-fill-mode: forwards;
+	animation-delay: 3.1s;
+}
+#PB {
+	position: absolute;
+	left: 456px;
+	top: 304px;
+	overflow: visible;
+	width: 20px;
+	white-space: nowrap;
+	text-align: left;
+	font-family: Muli;
+	font-style: normal;
+	font-weight: bold;
+	font-size: 14px;
+	color: rgba(255,255,255,1);
+	animation-name: example22;
+    animation-duration: .7s;animation-fill-mode: forwards;animation-timing-function: ease;animation-delay: 0.8s;
+}
+@keyframes example22 {
+  0%   {left:596px; top:304px;}
+  100% {left:456px; top:304px;}
+}
+#Priyanshu_Barnwal {
+	position: absolute;
+	left: 524px;
+	top: 273px;
+	overflow: visible;
+	width: 269px;opacity: 0;
+	white-space: nowrap;
+	text-align: left;
+	font-family: Muli;
+	font-style: normal;
+	font-weight: bold;
+	font-size: 30px;
+	color: rgba(35,35,35,1);animation:signup-response 0.5s 1;
+    -webkit-animation:signup-response 0.5s 1;
+    animation-fill-mode: forwards;animation-delay: 2.0s;animation-timing-function: ease;
+}
+@keyframes signup-response{
+    from {opacity :0;left: 500px;top: 273px;}
+    to {opacity :1;left: 524px;top: 273px;}
+}
+
+
+@media (max-width: 450px) {
+
+.Rectangle_468 {
+	position: absolute;
+	overflow: visible;
+	width: 370px;
+	height: 20px;
+	left: 535px;
+	top: 265px;
+}
+#Priyanshu_Barnwal {
+	position: absolute;
+	left: 549px;
+	top: 273px;
+	overflow: visible;
+	width: 269px;opacity: 0;
+	white-space: nowrap;
+	text-align: left;
+	font-family: Muli;
+	font-style: normal;
+	font-weight: bold;
+	font-size: 25px;
+	color: rgba(35,35,35,1);animation:signup-response 0.5s 1;
+    -webkit-animation:signup-response 0.5s 1;
+    animation-fill-mode: forwards;animation-delay: 2.0s;animation-timing-function: ease;
+}
+@keyframes signup-response{
+    from {opacity :0;left: 525px;top: 273px;}
+    to {opacity :1;left: 549px;top: 273px;}
+}
+
+#Digital_Product_Designer {
+	position: absolute;
+	left: 549px;
+	top: 280px;
+	overflow: visible;
+	width: 332px;
+	white-space: nowrap;
+	text-align: left;
+	font-family: Muli;z-index: -1;
+	font-style: normal;
+	font-weight: lighter;
+	font-size: 25px;
+	color: rgba(35,35,35,1);animation-name: example;
+    animation-duration: .6s;
+    animation-fill-mode: forwards; animation-delay: 2.3s;
+}
+
+@keyframes example {
+  0%   {left:549px; top:270px;}
+  100% {left:549px; top:314px;}
+}
+
+.Rectangle_467 {
+	position: absolute;
+	overflow: visible;
+	width: 4px;
+	height: 4px;
+	left: 100%;
+	top: 100%;
+	animation-name: example1;
+	animation-duration: .2s;
+	animation-fill-mode: forwards;
+	animation-delay: 1.5s;
+}
+@keyframes example1 {
+  0%   {left:458px; top:280px;}
+  100% {left:468px; top:290px;}
+}
+.Rectangle_418 {
+	filter: drop-shadow(0px 3px 6px rgba(0, 0, 0, 0.161));
+	position: absolute;
+	overflow: visible;
+	width: 62px;
+	height: 62px;
+	left: 100%;
+	top: 100%;
+	animation-name: example2;
+    animation-duration: .7s;animation-fill-mode: forwards;animation-delay: 0.8s;
+}
+@keyframes example2 {
+  0%   { outline-color: blue; outline-width: 1px; left:598px; top:280px;}
+  100% { fill: blue; left:458px; top:280px;}
+}
+
+#PB {
+	position: absolute;
+	left: 481px;
+	top: 304px;
+	overflow: visible;
+	width: 20px;
+	white-space: nowrap;
+	text-align: left;
+	font-family: Muli;
+	font-style: normal;
+	font-weight: bold;
+	font-size: 14px;
+	color: rgba(255,255,255,1);
+	animation-name: example22;
+    animation-duration: .7s;animation-fill-mode: forwards;animation-timing-function: ease;animation-delay: 0.8s;
+}
+@keyframes example22 {
+  0%   {left:621px; top:304px;}
+  100% {left:481px; top:304px;}
+}
+
+}
